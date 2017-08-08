c30d60e6a936bc5b66b0f71576f5913a63747f39
mfmunic <mfmunic@gmail.com>
2017-08-07 19:15:34 -0700

    api routes gets json

--- app/routes/view-routes.js
+++ app/routes/view-routes.js
@@ -1,15 +1,22 @@
 // Routes
 
-var spoon = require("../config/connection.js")
+var spoon = require("../Controllers/APIController.js")
 // =============================================================
 module.exports = function(app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/", function(req, res) {
-  	console.log(res)
-    res.json(res);
+  app.get("/api/data", function(req, res) {
+  	var mealPlan = spoon.getComputeDailyMealPlan(null, null, function(spoonErr, spoonRes){
+  		if (spoonErr){
+  			console.log(spoonErr)
+  			res.json(spoonErr) 
+  		}
+  		console.log(spoonRes)
+    	res.json(spoonRes);
+  	})
+  	
   });
 
 };
