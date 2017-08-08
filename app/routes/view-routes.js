bf69b649350f799ed4d908ffdd6e0c88f88c3190
mfmunic <mfmunic@gmail.com>
2017-08-07 19:40:34 -0700

    started html routes

--- app/routes/view-routes.js
+++ app/routes/view-routes.js
@@ -1,22 +1,15 @@
 // Routes
-
-var spoon = require("../Controllers/APIController.js")
+var path = require("path");
 // =============================================================
 module.exports = function(app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/api/data", function(req, res) {
-  	var mealPlan = spoon.getComputeDailyMealPlan(null, null, function(spoonErr, spoonRes){
-  		if (spoonErr){
-  			console.log(spoonErr)
-  			res.json(spoonErr) 
-  		}
-  		console.log(spoonRes)
-    	res.json(spoonRes);
-  	})
-  	
+  app.get("/meal-plan", function(req, res) {
+    console.log("test")
+    res.sendFile(path.join(__dirname, "../public/meal-plan.html"));
+
   });
 
 };
