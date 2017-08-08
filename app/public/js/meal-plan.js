2297b4e20c88078a16f71924c4e1c6635186249c
mfmunic <mfmunic@gmail.com>
2017-08-08 10:41:54 -0700

    added recipe api and recipe modals

--- app/public/js/meal-plan.js
+++ app/public/js/meal-plan.js
@@ -1,5 +1,46 @@
 $.get("/api/meal-plan", function(data) {
-    $("#breakfast").text(data.meals[0].title)
-    $("#lunch").text(data.meals[1].title)
-    $("#dinner").text(data.meals[2].title)
+	for (i=0; i<data.items.length/3; i++){
+		var meal = i*3
+		var day = i+1
+
+		var breakfast = JSON.parse(data.items[meal].value)
+		var lunch = JSON.parse(data.items[meal+1].value)
+		var dinner = JSON.parse(data.items[meal+2].value)
+
+		$("#breakfast"+day).text(breakfast.title)
+		$("#breakfast"+day).attr("recipe", breakfast.id)
+
+    	$("#lunch"+day).text(lunch.title)
+		$("#lunch"+day).attr("recipe", lunch.id)
+
+    	$("#dinner"+day).text(dinner.title)
+		$("#dinner"+day).attr("recipe", dinner.id)
+
+	}  
 });
+
+$(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+});
+
+$(".modal-trigger").on("click", function(){
+	$.get("/api/recipe/"+$(this).attr("recipe"), function(data) {
+		$("#modTitle").text(data.title)
+		$("#modImg").attr("src", data.image)
+		for(j=0;j<data.extendedIngredients.length;j++){
+			$("#ingList").append("<li>"+data.extendedIngredients[j].originalString)
+		}
+		for(k=0;k<data.analyzedInstructions[0].steps.length;k++){
+			$("#recList").append("<li>"+data.analyzedInstructions[0].steps[k].step)
+		}
+	});
+})
+
+//needs to reset modal on close or just adds to last recipe.
+$("modal-close").on("click", function(){
+	$("#modTitle").text("")
+	$("#modImg").attr("src", "#")
+	$("#ingList").removeChild()
+	$("recList").removeChild()
+})
--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -11,6 +11,7 @@ var spoon = require("../Controllers/APIController.js")
 // Routes
 // =============================================================
 module.exports = function(app) {
+
   app.get("/api/meal-plan", function(req, res) {
 
       var mealPlan = spoon.getComputeDailyMealPlan(null, "week", function(spoonErr, spoonRes){
@@ -19,8 +20,24 @@ module.exports = function(app) {
         console.log(spoonErr)
         res.json(spoonErr) 
       }
-      console.log(spoonRes)
       res.json(spoonRes);
     })
   });
+
+  app.get("/api/recipe/:id", function(req, res){
+
+    var recipeId = req.params.id
+    console.log(req.params.id)
+    var recipe = spoon.getRecipeInformation(recipeId, function(spoonErr, spoonRes){
+
+      if (spoonErr){
+        console.log(spoonErr)
+        res.json(spoonErr) 
+      }
+      console.log(spoonRes)
+      res.json(spoonRes);
+    })
+      
+  })
+    
 };
