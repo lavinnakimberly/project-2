08baf2e70e67231171b5df59687ef18de26c1091
lavinnakimberly <lavinnakimberly@gmail.com>
2017-08-10 20:16:04 -0700

    updated meal plan page

--- app/public/js/meal-plan.js
+++ app/public/js/meal-plan.js
@@ -53,3 +53,11 @@ $(".modal-trigger").on("click", function(){
 $(".modal-close").on("click", function(){
 	$(".modal-content").empty()
 })
+
+ $('.carousel.carousel-slider').carousel({fullWidth: true});
+
+
+  // Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
